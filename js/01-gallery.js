a03a0b212f80e89ccf8980d72de5f0c534c7a3c4
Serhii Haber <haberserhii@icloud.com>
2023-08-05 13:35:42 +0300

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryListItem = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      width="250px"
+    />
+  </a>
+</li>
+`
+    )
+    .join("");
+}
+galleryListItem.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+const galleryLink = document.querySelector(".gallery__link");
+galleryListItem.addEventListener("click", function (clickEvent) {
+  clickEvent.preventDefault();
+  if (clickEvent.target === clickEvent.currentTarget) {
+    return;
+  }
+  const bigImage = galleryItems.find(
+    ({ original }) => original === clickEvent.target.dataset.source
+  );
+  const modalWindow = basicLightbox.create(
+    `<div class="modal">
+    <img src="${bigImage.original}" alt="${bigImage.description}" width="800px">
+    </div>`
+  );
+  modalWindow.show();
+
+  const escapeWindow = (escEvent) => {
+    if (escEvent.key === "Escape") {
+      modalWindow.close();
+      document.removeEventListener("keydown", escapeWindow);
+    }
+  };
+  document.addEventListener("keydown", escapeWindow);
+});
