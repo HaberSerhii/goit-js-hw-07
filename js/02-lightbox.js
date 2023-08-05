1066c993e27232ba8dee45f3ae4a1e6583ea8e48
Serhii Haber <haberserhii@icloud.com>
2023-08-05 14:03:15 +0300

    Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryListItem = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      width="250px"
+    />
+  </a>
+</li>
+`
+    )
+    .join("");
+}
+galleryListItem.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+// galleryListItem.addEventListener("click", function (clickEvent) {
+//   clickEvent.preventDefault();
+//   if (clickEvent.target === clickEvent.currentTarget) {
+//     return;
+//   }
+//   new SimpleLightbox(".gallery__item a", {
+//     captionsData: "alt",
+//     captionPosition: "bottom",
+//     captionDelay: 250,
+//   });
+// });
+
+const lightBox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
